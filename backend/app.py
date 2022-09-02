bef5ce864a5588b0ea302ee89ff3e388d6e1911c
Zhu <57535849+DerekZZhu@users.noreply.github.com>
2022-09-02 16:09:50 -0700

    Created API resource paths for Top Trips and processed trip plans

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,28 @@
 from flask import Flask
+from flask_restful import Resource, Api
 
-app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+app = Flask(__name__)
 
+api = Api(app)
+searches = []
+
+class TopTrips(Resource):
+
+    def get(self):
+        return {"top_trips": ["Beijing", "Los Angeles", "Tokyo"]}
+
+
+class ProcessedPlan(Resource):
+
+    def post(self, city_name):
+        search = {"searched":city_name}
+        searches.append(search)
+        return search
+
+# Top Trips (Returning most popular trips near you)
+api.add_resource(TopTrips, '/trips')
+# Returns PROCESSED TRIP PLAN (AI Data)
+api.add_resource(ProcessedPlan, '/plans/<string:city_name>')
 
 # POST /user data: {tripName:}
 @app.route('/trip_plan', methods=["POST"])
@@ -15,21 +36,9 @@ def get_attractions(city_name):
     pass
 
 
-# GET /Best planned Attractions
-@app.route('/trips')
-def get_top_trips():
-    return {"top_trips": ["Beijing", "Los Angeles", "Tokyo"]}
-
-
 @app.route('/attractions/<string:city_name>/place')
 def get_attractions_place(name):
     pass
-
-
-@app.route('/')
-def serve():  # put application's code here
-    return '<h1>Welcome To Wanderlag!</h1>'
-
 
 if __name__ == '__main__':
     app.run(debug=True)
