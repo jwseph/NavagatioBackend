39ef2bbbf729bc298be25cfa4ab7c8aca39b751c
Zhu <57535849+DerekZZhu@users.noreply.github.com>
2022-09-03 13:41:21 -0700

    Added world citites data and processed autocomplete data

--- backend/components/autocomplete.py
+++ backend/components/autocomplete.py
@@ -5,8 +5,9 @@ from trie import Trie
 test_words = ['Foo', 'Fosh', 'Big', 'Bibble', 'Blasted']
 autocompleter = Trie()
 
-# with open('places.json', 'w') as f:
-#     json.dump(autocompleter.to_dict(), f)
+
 
 autocompleter.addlist(test_words)
-print(f'With prefix B {autocompleter.autocomplete("Bi")}')
+with open('places.json', 'w') as f:
+    json.dump(autocompleter.to_dict(), f)
+# print(f'With prefix B {autocompleter.autocomplete("Bi")}')
