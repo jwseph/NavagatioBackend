5bd5781585a4169247d5c07e9c7b83c9d26bbc92
Zhu <57535849+DerekZZhu@users.noreply.github.com>
2022-09-05 19:07:46 -0700

    Updated form structure + buttons

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -1,7 +1,7 @@
 import '../sass/button.scss';
 function Button(props) {
     return(
-        <a href="#" className='btn btn-gray'>{props.children}</a>
+        <a href="#" className={ `btn ${props.theme}` }>{props.children}</a>
     )
 }
 
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { useSignup } from "../hooks/useSignup";
-import { MdMail, MdLock } from 'react-icons/md';
-import { AiFillEyeInvisible } from 'react-icons/ai';
+import { MdMail, MdLock, MdFacebook,  } from 'react-icons/md';
+import { FcGoogle} from 'react-icons/fc';
+import { AiFillEyeInvisible, AiFillApple } from 'react-icons/ai';
 
 import Button from '../components/Button';
 import "./Auth.scss"
@@ -20,38 +21,45 @@ export default function Signup() {
         <div>
             <h1>Get Started</h1>
             <p>Please create an account by email</p>
-            <form method="post" onSubmit={handleSubmit}>
-                <div className="input-field">
-                    <MdMail className="icon"/>
-                    <div className="content-field">
-                        <input type="text" name="email" onChange={(event)=>{setEmail(event.target.value)}} required/>
-                        <span className="highlight"></span>
-                        <label>Email</label>
-                    </div>
-                </div>
+                <form method="post" onSubmit={handleSubmit}>
+                    <div className="form-container">
+                        <div className="input-field">
+                            <MdMail className="icon"/>
+                            <div className="content-field">
+                                <input type="text" name="email" onChange={(event)=>{setEmail(event.target.value)}} required/>
+                                <span className="highlight"></span>
+                                <label>Email</label>
+                            </div>
+                        </div>
 
-                <div className="input-field">
-                    <MdLock className="icon"/>
-                    <div className="content-field">
-                        <input type="password" onChange={(event)=>{setPassword(event.target.value)}} required/>
-                        <span className="highlight"></span>
-                        <label>Password</label>
-                    </div>
-                </div>
+                        <div className="input-field">
+                            <MdLock className="icon"/>
+                            <div className="content-field">
+                                <input type="password" onChange={(event)=>{setPassword(event.target.value)}} required/>
+                                <span className="highlight"></span>
+                                <label>Password</label>
+                            </div>
+                        </div>
 
-                <div className="input-field">
-                    <MdLock className="icon"/>
-                    <div className="content-field">
-                        <input type="password" required/>
-                        <span className="highlight"></span>
-                        <label>Confirm password</label> 
+                        <div className="input-field">
+                            <MdLock className="icon"/>
+                            <div className="content-field">
+                                <input type="password" required/>
+                                <span className="highlight"></span>
+                                <label>Confirm password</label> 
+                            </div>
+                        </div>
                     </div>
-                </div>
-
-                <p>Already have an account?<span>Sign in</span></p>
-                <Button>Sign Up</Button>
-                {error && <p>{error}</p>}
-            </form>
+                    <p>Already have an account?<span>Log in</span></p>
+                    {/* <input type="submit" value="Submit"/> */}
+                    {error && <p>{error}</p>}
+                </form>
+            <div>
+                <span/><p>or</p><span/>
+            </div>
+            <Button theme='btn-white'><FcGoogle/>Continue with Google</Button>
+            <Button theme='btn-black'><AiFillApple/>Continue with Apple</Button>
+            <Button theme='btn-blue'><MdFacebook/>Continue with Facebook</Button>
         </div>
     )
 }
