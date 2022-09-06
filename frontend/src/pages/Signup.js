35612b04010a7179536b4932c7cfb61472fd6029
Zhu <57535849+DerekZZhu@users.noreply.github.com>
2022-09-05 21:26:06 -0700

    Added password visibility and button container

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -2,19 +2,25 @@ import { useState } from "react";
 import { useSignup } from "../hooks/useSignup";
 import { MdMail, MdLock, MdFacebook,  } from 'react-icons/md';
 import { FcGoogle} from 'react-icons/fc';
-import { AiFillEyeInvisible, AiFillApple } from 'react-icons/ai';
+import { AiFillEyeInvisible, AiFillEye, AiFillApple } from 'react-icons/ai';
 
 import Button from '../components/Button';
-import "./Auth.scss"
+import '../sass/button.scss';
+import "./Auth.scss";
 
 export default function Signup() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [visibility, setVisibility] = useState(false)
     const { error, signup } = useSignup()
 
     const handleSubmit = (event) => {
         event.preventDefault()
         signup(email, password)
+    }
+
+    const toggleVision = () => {
+        setVisibility(!visibility)
     }
 
     return(
@@ -35,31 +41,35 @@ export default function Signup() {
                         <div className="input-field">
                             <MdLock className="icon"/>
                             <div className="content-field">
-                                <input type="password" onChange={(event)=>{setPassword(event.target.value)}} required/>
+                                <input type={visibility?"text":"password"} onChange={(event)=>{setPassword(event.target.value)}} required/>
                                 <span className="highlight"></span>
                                 <label>Password</label>
+                                {visibility?<AiFillEye className="eye-con" onClick={toggleVision}/>:<AiFillEyeInvisible className="eye-con" onClick={toggleVision}/>}
                             </div>
                         </div>
 
                         <div className="input-field">
                             <MdLock className="icon"/>
                             <div className="content-field">
-                                <input type="password" required/>
+                                <input type={visibility?"text":"password"} required/>
                                 <span className="highlight"></span>
-                                <label>Confirm password</label> 
+                                <label>Confirm password</label>
+                                {visibility?<AiFillEye className="eye-con" onClick={toggleVision}/>:<AiFillEyeInvisible className="eye-con" onClick={toggleVision}/>}
                             </div>
                         </div>
                     </div>
                     <p>Already have an account?<span>Log in</span></p>
-                    {/* <input type="submit" value="Submit"/> */}
+                    <input type="submit" value="Submit" className="btn btn-gray"/>
                     {error && <p>{error}</p>}
                 </form>
             <div>
                 <span/><p>or</p><span/>
             </div>
-            <Button theme='btn-white'><FcGoogle/>Continue with Google</Button>
-            <Button theme='btn-black'><AiFillApple/>Continue with Apple</Button>
-            <Button theme='btn-blue'><MdFacebook/>Continue with Facebook</Button>
+            <div className="button-container">
+                <Button theme='btn-white'><FcGoogle/>Continue with Google</Button>
+                <Button theme='btn-black'><AiFillApple/>Continue with Apple</Button>
+                <Button theme='btn-blue'><MdFacebook/>Continue with Facebook</Button>
+            </div>
         </div>
     )
 }
