d9f57044bab0e6859e53e70889ee585da5caee48
Zhu <57535849+DerekZZhu@users.noreply.github.com>
2022-09-06 23:07:39 -0700

    Added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,20 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import TripList from './layout/TripList';
 import Signup from './pages/Signup';
-import Login from './pages/Login';
+import Login from './pages/Home';
 import SearchBar from './components/SearchBar';
 
 import './App.css';
 
 function App() {
-  // useEffect(() => {
-  //   fetch("/search/Muki").then(
-  //     res => res.json()
-  //   ).then(
-  //     data => {
-  //       setKismet(data)
-  //       console.log(data);
-  //     }
-  //   )
-  // }, [])
-
-  // console.log(attractions)
   return (
-    <div className="App">
-      <Signup/>
-      {/* <SearchBar/> */}
-    </div>
+      <Routes>
+        <Route element={<Login />}/>
+        <Route path="/" element={<Nav />}>
+          <Route index element={<Home/>}/>
+        </Route>
+      </Routes>
   );
 }
 
