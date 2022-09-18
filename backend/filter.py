f2eaa81fac0370b1afd47f7c17123e5bf83fd48e
Zhu <57535849+DerekZZhu@users.noreply.github.com>
2022-09-17 18:18:17 -0700

    Filtering process working

--- backend/filter.py
+++ backend/filter.py
@@ -1,5 +1,5 @@
 import json
-def user_pref(preferences=['Landmarks']):
+def user_pref(preferences=['tourist_attraction', 'park']):
     user_prefs = {}
     for preference in preferences:
         user_prefs[preference] = True
@@ -12,9 +12,12 @@ def filtering(data, user_prefs):
         attraction_types = attraction['basic_info']['candidates'][0]['types']
         for _type in attraction_types:
             if user_prefs.get(_type) != None:
-                matched_tags[attraction['basic_info']['candidates'][0]['name']] += 1
+                if matched_tags.get(attraction['basic_info']['candidates'][0]['name']) != None:
+                   matched_tags[attraction['basic_info']['candidates'][0]['name']]+=1
+                else:
+                    matched_tags[attraction['basic_info']['candidates'][0]['name']] = 1 
 
-    return sorted(matched_tags.items(), key=lambda x: x[1])
+    return sorted(matched_tags.items(), key=lambda x: x[1], reverse=True)
     
-dicts = json.loads(open(r'backend\Mukilteo-Attraction-Data.json', 'rb').read().decode('utf-8'))
+dicts = json.loads(open(r'data\Seoul-Attraction-Data.json', 'rb').read().decode('utf-8'))
 print(filtering(dicts, user_pref()))
